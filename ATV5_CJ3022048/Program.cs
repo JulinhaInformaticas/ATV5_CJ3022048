3e345f48e538fff3285ab56addde0116841bcc99
CJ3022048 <CJ3022048@LAB01-PC21>
2024-03-27 10:44:50 -0300

    Adicionar arquivos de projeto.

---
+++ ATV5_CJ3022048/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace ATV5_CJ3022048
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcgeral;
+
+            Console.WriteLine("**MENU DE ATIVIDADES** \n 1-ATIVIDADE DE SALA \n 2-EXERCÍCIO 1 \n 3-EXERCÍCIO 2 \n 4-EXERCÍCIO 3 \n 5-EXERCÍCIO 4 \n 6-EXERCÍCIO 5 \n 7-EXERCÍCIO 6 \n 8-EXERCÍCIO 7");
+            opcgeral = int.Parse(Console.ReadLine());
+
+            switch (opcgeral)
+            {
+                case 1:
+                    Console.WriteLine("ATIVIDADE DE SALA");
+                    //ATV SALA
+                    int a, temp;
+                    int soma = 0;
+
+                    for (a = 0; a < 3; a++)
+                    {
+                        Console.WriteLine("digite um valor: ");
+                        temp = int.Parse(Console.ReadLine());
+                        if (temp < 0)
+                        {
+                            break;
+                        }
+                        soma = soma + temp;
+                    }
+                    Console.WriteLine("a medida é: {0}", soma / 3);
+                    break;
+
+                case 2:
+                    Console.WriteLine("ATIVIDADE 1");
+                    //ATV 1
+                    int n, m;
+
+                    Console.WriteLine("digite um número inteiro aleatório: ");
+                    m = int.Parse(Console.ReadLine());
+
+                    for (n = 1; n <= m; n++)
+                        Console.WriteLine("{0}", n);
+                break;
+
+                case 3:
+                    Console.WriteLine("ATIVIDADE 2");
+                    //ATV 2
+                    int n1, m1;
+                    int par;
+
+                    Console.WriteLine("digite um número inteiro aleatório: ");
+                    m1 = int.Parse(Console.ReadLine());
+
+                    for (par = 0; par <= n1; n1++) ;
+                       if (n1 %2 ==0)
+                    {
+                        Console.WriteLine("{0}", n1);
+                    }
+
+
+                break;
+
+                case 4:
+                    //ATV 3
+
+                    break;
+
+                case 5:
+                    //ATV 4
+
+                    break;
+
+                case 6:
+                    //ATV 5
+
+                    break;
+
+                case 7:
+                    Console.WriteLine("ATIVIDADE 6");
+                    //ATV 6
+                    int v1 , v2, v3, v4, v5, v6, v7, v8, v9, v10;
+
+                    Console.WriteLine("digite um valor para v1: ");
+                    v1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("digite um valor para v2: ");
+                    v2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("digite um valor para v3: ");
+                    v3 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("digite um valor para v4: ");
+                    v4 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("digite um valor para v5: ");
+                    v5 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("digite um valor para v6: ");
+                    v6 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("digite um valor para v7: ");
+                    v7 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("digite um valor para v8: ");
+                    v8 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("digite um valor para v9: ");
+                    v9 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("digite um valor para v10: ");
+                    v10 = int.Parse(Console.ReadLine());
+                  
+
+                    break;
+
+                case 8:
+                    //ATV 7
+                    
+
+                    break;
+
+                default:
+                    Console.WriteLine("opção inválida");
+                    break;
+            }
+        }
+    }
+}
